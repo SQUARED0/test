2b2d47f8e0ffcef624c6a37c3ce604df66e3a0c4
SQUARED <141570367+SQUARED0@users.noreply.github.com>
2023-09-19 19:54:04 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,56 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event1_Act1(runtime, localVars)
+	{
+		screen = 3;
+		
+	},
+
+	async Egame_Event1_Act2(runtime, localVars)
+	{
+		localStorage.setItem('screen', '3');
+	},
+
+	async Egame_Event1_Act6(runtime, localVars)
+	{
+		localStorage.setItem('color', '#fcf99b');
+	},
+
+	async Egame_Event1_Act9(runtime, localVars)
+	{
+		runtime.globalVars.per2 = localStorage.getItem('screenshot2');
+	},
+
+	async Egame_Event9_Act1(runtime, localVars)
+	{
+		runtime.globalVars.per = localStorage.getItem('screenshot');
+	},
+
+	async Egame_Event17_Act2(runtime, localVars)
+	{
+		localStorage.setItem('color', runtime.globalVars.HEXcolor);
+	},
+
+	async Egame_Event19_Act2(runtime, localVars)
+	{
+		screen = 3;
+		
+	},
+
+	async Egame_Event19_Act3(runtime, localVars)
+	{
+		localStorage.setItem('screen', '3');
+	},
+
+	async Egame_Event27(runtime, localVars)
+	{
+		runtime.globalVars.Log = localStorage.getItem('log');
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
